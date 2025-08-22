37b26afeaaaaf8d0fdfda3fcd6698a99a8190629
mgoldberg10 <matthew.goldberg10@gmail.com>
2025-08-22 18:35:40 -0500

    Updating plotly figure, removing old html and adding new. This can't be the best way right?

--- _notebooks/smart_aste_load.py
+++ _notebooks/smart_aste_load.py
@@ -5,6 +5,7 @@ sys.path.append('/Users/mattgoldberg/Projects/CSEM/research/CRIOS/SMART/smart_ca
 from utils import *
 import matplotlib.pyplot as plt
 import os
+import cmocean
 
 import pandas as pd
 from plotting import express as px
@@ -12,18 +13,31 @@ from plotting import graph_objects as go
 
 # Base directory relative to the notebook/post
 base_dir = os.path.join(os.getcwd(), "assets", "data", "aste")   
-nx, ny = (250, 150)
-x = read_float32(os.path.join(base_dir, 'x.bin')).reshape((nx, ny))
-y = read_float32(os.path.join(base_dir, 'y.bin')).reshape((nx, ny))
-z = read_float32(os.path.join(base_dir, 'z.bin')).reshape((nx, ny))
-df = pd.read_csv(os.path.join(base_dir, 'sc_aste.csv'))
+
+nx, ny = (135, 400)
+#nx, ny = (55, 120)
+
+base_dir = os.path.join(os.getcwd(), "assets", "data", "plotly_grid")   
+grid_path = os.path.join(base_dir, 'plotly_spna_grid.bin')
+
+x, y, z = np.fromfile(grid_path, dtype='<f4').reshape(3, nx, ny)
+z = -z
+
+
+#x = read_float32(os.path.join(base_dir, 'x.bin')).reshape((nx, ny))
+#y = read_float32(os.path.join(base_dir, 'y.bin')).reshape((nx, ny))
+#z = read_float32(os.path.join(base_dir, 'z.bin')).reshape((nx, ny))
+df = pd.read_csv(os.path.join(base_dir, 'spna_sensors_depths.csv'))
+df['Depth'] *= -1
 
 # Normalize/filter z
 #z_filtered = np.exp(z / (np.max(z) + 1e-10))
 #z = z_filtered.copy()
 
 # Build custom blue colorscale
-blues = plt.cm.get_cmap('Blues')
+#blues = plt.cm.get_cmap('Blues')
+blues = cmocean.cm.deep
+
 blues_r = blues.reversed()
 colorscale = []
 for i in range(blues_r.N):
--- _posts/2025-08-22-spna_cables.md
+++ _posts/2025-08-22-spna_cables.md
@@ -5,15 +5,11 @@ date: 2025-08-22
 comments: true
 toc: true
 ---
-
-
 <iframe style="color-scheme: none;"
     scrolling="no"
     width="920px"
     height="820"
-    src="/assets/notebooks/html_d98a5c61-0797-4469-b0a1-37835ecd1485/figure_0.html"
+    src="/assets/notebooks/html_34534ab3-766f-4f6b-8641-b94685b6e846/figure_0.html"
     frameborder="0"
     allowfullscreen
 ></iframe>
-
-
